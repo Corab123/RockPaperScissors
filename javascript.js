16b21f5db4b5a16528e005e15f2e886cc2460bf4
Ákos Balogh <akos.balogh87@gmail.com>
2022-05-11 16:50:00 +0200

    Correct game logic

--- javascript.js
+++ javascript.js
@@ -1,35 +1,52 @@
-const guesses = ['Rock', 'Paper', 'Scissors']
+
+const guesses = ['ROCK', 'PAPER', 'SCISSORS']
+
 function computerPlay(){
-    return guesses[math.floor(math.random()*3)]; 
+    return guesses[Math.floor(Math.random()*3)]; 
 }
 
-function playRound(computerGuess, playerGuess) {
-    if (computerGuess === 'Rock') {
-        if (playerGuess === 'Rock') {
-            console.log("Both players chose ${playerGuess}. It's a tie.");
-        } else if (playerGuess === 'Paper') {
-            console.log("Player wins with ${playerGuess}");
+function playRound(playerGuess) {
+    const computerGuess = computerPlay();
+    playerGuess = checkMove(playerGuess);
+    console.log(`Computer guess: ${computerGuess}.`);
+    console.log(`Player guess: ${playerGuess}`);
+    if (computerGuess === 'ROCK') {
+        if (playerGuess === 'ROCK') {
+            console.log(`Both players chose ${playerGuess}. It's a tie.`);
+        } else if (playerGuess === 'PAPER') {
+            console.log(`Player wins with ${playerGuess}`);
         } else {
-            console.log("Computer wins with ${ComputerGuess}");
+            console.log(`Computer wins with ${computerGuess}`);
         }
-    } else if (computerGuess === 'Paper') {
-        if (playerGuess === 'Paper') {
-            console.log("Both players chose ${playerGuess}. It's a tie.");
-        } else if (playerGuess === 'Scissors') {
-            console.log("Player wins with ${playerGuess}");
+    } else if (computerGuess === 'PAPER') {
+        if (playerGuess === 'PAPER') {
+            console.log(`Both players chose ${playerGuess}. It's a tie.`);
+        } else if (playerGuess=== 'SCISSORS') {
+            console.log(`Player wins with ${playerGuess}`);
         } else {
-            console.log("Computer wins with ${ComputerGuess}");
+            console.log(`Computer wins with ${computerGuess}`);
         }
-    } else {
-        if (playerGuess === 'Scissors') {
-            console.log("Both players chose ${playerGuess}. It's a tie.");
-        } else if (playerGuess === 'Paper') {
-            console.log("Player wins with ${playerGuess}");
+    } else if (computerGuess === 'SCISSORS') {
+        if (playerGuess === 'SCISSORS') {
+            console.log(`Both players chose ${playerGuess}. It's a tie.`);
+        } else if (playerGuess === 'ROCK') {
+            console.log(`Player wins with ${playerGuess}`);
         } else {
-            console.log("Computer wins with ${ComputerGuess}");
+            console.log(`Computer wins with ${computerGuess}`);
         }
     }
 }
 
-const computerGuess = computerPlay();
-const playerGuess = 1;
+function checkMove(playerGuess){
+    if (guesses.includes(playerGuess.toUpperCase())) {
+        return playerGuess.toUpperCase();
+    } else {
+        console.log("Invalid guess")
+        return
+    }
+}
+
+function game();
+    for (let i = 0; i < 5; i++){
+        playRound()
+    }
