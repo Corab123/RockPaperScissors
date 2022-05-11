75796c95e005d0be0df48e5fbea0188b13aa782c
Ákos Balogh <akos.balogh87@gmail.com>
2022-05-11 17:05:37 +0200

    Add 5 match long game function

--- javascript.js
+++ javascript.js
@@ -5,34 +5,44 @@ function computerPlay(){
     return guesses[Math.floor(Math.random()*3)]; 
 }
 
-function playRound(playerGuess) {
+function playRound() {
     const computerGuess = computerPlay();
+    playerGuess = prompt("Enter your guess: ","Rock");
     playerGuess = checkMove(playerGuess);
     console.log(`Computer guess: ${computerGuess}.`);
     console.log(`Player guess: ${playerGuess}`);
     if (computerGuess === 'ROCK') {
         if (playerGuess === 'ROCK') {
             console.log(`Both players chose ${playerGuess}. It's a tie.`);
+            return 0;
         } else if (playerGuess === 'PAPER') {
             console.log(`Player wins with ${playerGuess}`);
+            return 1;
         } else {
             console.log(`Computer wins with ${computerGuess}`);
+            return -1;
         }
     } else if (computerGuess === 'PAPER') {
         if (playerGuess === 'PAPER') {
             console.log(`Both players chose ${playerGuess}. It's a tie.`);
+            return 0;
         } else if (playerGuess=== 'SCISSORS') {
             console.log(`Player wins with ${playerGuess}`);
+            return 1;
         } else {
             console.log(`Computer wins with ${computerGuess}`);
+            return -1;
         }
     } else if (computerGuess === 'SCISSORS') {
         if (playerGuess === 'SCISSORS') {
             console.log(`Both players chose ${playerGuess}. It's a tie.`);
+            return 0;
         } else if (playerGuess === 'ROCK') {
             console.log(`Player wins with ${playerGuess}`);
+            return 1;
         } else {
             console.log(`Computer wins with ${computerGuess}`);
+            return -1;
         }
     }
 }
@@ -46,7 +56,23 @@ function checkMove(playerGuess){
     }
 }
 
-function game();
+function game(){
+let playerScore = 0
+let computerScore = 0
     for (let i = 0; i < 5; i++){
-        playRound()
+        roundScore = playRound()
+        if (roundScore===1) {
+            playerScore++
+        } else if (roundScore ===-1) {
+            computerScore++
+        }
+        console.log(`Player: ${playerScore} ; Computer: ${computerScore}`)
     }
+    if (computerScore === playerScore) {
+        console.log("The game is a tie!")
+    } else if (playerScore>computerScore) {
+        console.log("Player won the game!")
+    } else {
+        console.log("Computer won the game!")
+    }
+}
